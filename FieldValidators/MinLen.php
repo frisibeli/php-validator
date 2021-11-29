97ea78997cfc5cae24dfd71defeaf836c6402888
Vanessa Stoynova <vanessa.stoynova98@gmail.com>
2021-11-29 11:25:25 +0200

    Create MinLen.php

---
+++ FieldValidators/MinLen.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'FieldValidator.php';
+
+class MinLen implements FieldValidator {
+  private $min = 0;
+
+  public function __construct($min) {
+    $this->min = $min;
+  }
+
+  public function isValid($value) {
+    return strlen($value) >= $this->min;
+  }
+}
+
+$validator = new MinLen(10);
+
+// echo $validator->isValid("dsfhds");
